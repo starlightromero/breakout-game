c5b831a9e56939afbbb85604d0834ae19b47caa6
Starlight Romero <starlightromero@gmail.com>
2020-10-21 19:03:05 -0700

    added mouse control

--- canvas.js
+++ canvas.js
@@ -33,6 +33,7 @@ for (let c = 0; c < brickColumnCount; c++) {
 
 document.addEventListener('keydown', keyDownHandler, false)
 document.addEventListener('keyup', keyUpHandler, false)
+document.addEventListener('mousemove', mouseMoveHandler, false)
 
 function keyDownHandler (e) {
   if (e.key === 'Right' || e.key === 'ArrowRight') {
@@ -47,6 +48,13 @@ function keyUpHandler (e) {
     rightPressed = false
   } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
     leftPressed = false
+  }
+}
+
+function mouseMoveHandler (e) {
+  const relativeX = e.clientX - canvas.offsetLeft
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2
   }
 }
 

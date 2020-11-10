c91cc48913b9022e2457be988aeef35d4c0caf10
Starlight Romero <starlightromero@gmail.com>
2020-11-09 16:19:34 -0800

    added PI2

--- Components/Ball.js
+++ Components/Ball.js
@@ -6,11 +6,12 @@ class Ball {
     this.dy = dy;
     this.radius = radius;
     this.color = color;
+    this.PI2 = Math.PI * 2;
   }
 
   render(ctx) {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, this.PI2);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();

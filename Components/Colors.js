c984e7eb283f25443ae7677c6d2ab83265cb048a
Starlight Romero <starlightromero@gmail.com>
2020-11-09 18:42:28 -0800

    added all colors as static methods

--- Components/Colors.js
+++ Components/Colors.js
@@ -1,16 +1,49 @@
 class Colors {
-  constructor() {
-    this.grey = '#dadada';
-    this.violet = '#CC00FF';
-    this.purple = '#8000FF';
-    this.blue = '#0000FF';
-    this.lightBlue = '#0080FF';
-    this.skyBlue = '#00FFFF';
-    this.blueGreen = '#00FF80';
-    this.green = '#00FF00';
-    this.limeGreen = '#80FF00';
-    this.yellow = '#FFFF00';
-    this.orange = '#FF8000';
-    this.red = '#FF0000';
+  static violet() {
+    return '#CC00FF';
+  }
+
+  static purple() {
+    return '#8000FF';
+  }
+
+  static blue() {
+    return '#0000FF';
+  }
+
+  static lightBlue() {
+    return '#0080FF';
+  }
+
+  static skyBlue() {
+    return '#00FFFF';
+  }
+
+  static blueGreen() {
+    return '#00FF80';
+  }
+
+  static green() {
+    return '#00FF00';
+  }
+
+  static limeGreen() {
+    return '#80FF00';
+  }
+
+  static yellow() {
+    return '#FFFF00';
+  }
+
+  static orange() {
+    return '#FF8000';
+  }
+
+  static red() {
+    return '#FF0000';
+  }
+
+  static grey() {
+    return '#dadada';
   }
 }
